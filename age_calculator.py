8d82bd24bf90c8ffa01ae6fcbd979b0ce942f47c
Thomas Wotoro <didymusthomas6@gmail.com>
2025-08-16 19:47:12 +0300

    Initial Release

---
+++ age_calculator.py
@@ -0,0 +1,15 @@
+# Simple Age Calculator 
+
+"""
+ask for user inputs
+check their input
+calculate their age
+return the age.
+
+"""
+
+user_age = int(input("Enter youe age: "))
+
+age = 2025 - user_age
+
+print(age)
