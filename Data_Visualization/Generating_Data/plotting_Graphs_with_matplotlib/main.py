b3cb4f46ba07b0c1f6c122b5114d7053fdd5427f
tom <didymusthomas6@gmail.com>
2025-12-01 10:53:16 +0300

    Update the ploting with more design

--- Data_Visualization/Generating_Data/plotting_Graphs_with_matplotlib/main.py
+++ Data_Visualization/Generating_Data/plotting_Graphs_with_matplotlib/main.py
@@ -11,7 +11,13 @@ def main():
 def plotting():
     squares = [1, 4, 9, 16, 25]
 
-    plt.plot(squares)
+    plt.plot(squares, linewidth=5)
+    plt.title("Square Numbers", fontsize=24)
+    plt.xlabel("Value", fontsize=14)
+    plt.ylabel("Square of Value", fontsize=14)
+
+    # Set size of tick labels
+    plt.tick_params(axis='both', labelsize=14)
     plt.show()
 
 
