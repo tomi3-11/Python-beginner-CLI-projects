a7a9b7f99e86448a207bb35dad731ca8fb4ed5e6
tom <didymusthomas6@gmail.com>
2025-12-01 11:40:16 +0300

    Update main.py

--- Data_Visualization/Generating_Data/plotting_Graphs_with_matplotlib/main.py
+++ Data_Visualization/Generating_Data/plotting_Graphs_with_matplotlib/main.py
@@ -9,9 +9,12 @@ def main():
 
 
 def plotting():
+    # Setting the input values as X axis
+    input_values = [1, 2, 3, 4, 5]
+    # setting the squares as Y axis
     squares = [1, 4, 9, 16, 25]
-
-    plt.plot(squares, linewidth=5)
+    # Plotting the x and y axis
+    plt.plot(input_values, squares, linewidth=5)
     plt.title("Square Numbers", fontsize=24)
     plt.xlabel("Value", fontsize=14)
     plt.ylabel("Square of Value", fontsize=14)
