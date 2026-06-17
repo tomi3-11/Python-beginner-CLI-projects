b8c335f3fcc8c5d59c64e0faeade45a711c07ff4
tom <didymusthomas6@gmail.com>
2026-06-17 14:59:53 +0300

    Update cli calculator with input validation

--- small_projects/cli_calculator.py
+++ small_projects/cli_calculator.py
@@ -1,16 +1,38 @@
 
 def main():
-    ans1 = add(4, 5)
-    print(f"The addition result is {ans1}")
+    #ans1 = add(4, 5)
+    #print(f"The addition result is {ans1}")
 
-    ans2 = subtract(5, 2)
-    print(f"The subtraction result is {ans2}")
+    #ans2 = subtract(5, 2)
+    #print(f"The subtraction result is {ans2}")
 
-    ans3 = multiply(5, 2)
-    print(f"The multiply result is {ans3}")
+    #ans3 = multiply(5, 2)
+    #print(f"The multiply result is {ans3}")
 
-    ans4 = divide(5, 2)
-    print(f"The divide result is {ans4}")
+    #ans4 = divide(5, 2)
+    #print(f"The divide result is {ans4}")
+
+
+    while True:
+        num1 = int(input("Enter the first name: "))
+        num2 = int(input("Enter the first name: "))
+        operator = input("Enter a valid operator (+, -, /, *): ")
+
+        if operator == "+":
+            print(add(num1, num2))
+        elif operator == "-":
+            print(subtract(num1, num2))
+        elif operator == "*":
+            print(multiply(num1, num2))
+        elif operator == "/":
+            print(divide(num1, num2))
+        else:
+            print("Invalid operation")
+
+        continue_cal = input("Do you want to continue? (y/n)").lower()
+        if continue_cal != "y":
+            print("Exiting cal")
+            break
 
 
 def add(num1: int, num2: int) -> int:
